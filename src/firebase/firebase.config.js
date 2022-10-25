760726ba9a89fa0d6397312ddba964cd4813090f
nafisnihal <nafisnihal12@gmail.com>
2022-10-25 21:35:16 +0600

    literate

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOXqX3SFvvtpbUWmk4yRtBHfn-BcCsS2Y",
+  authDomain: "literate-d3e2f.firebaseapp.com",
+  projectId: "literate-d3e2f",
+  storageBucket: "literate-d3e2f.appspot.com",
+  messagingSenderId: "325451877505",
+  appId: "1:325451877505:web:c8b12a35f6560dcba15180",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
