3885457ed5f5617375258b7a3e8ccded7aae24d8
nafisnihal <nafisnihal12@gmail.com>
2022-10-27 23:25:27 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAOXqX3SFvvtpbUWmk4yRtBHfn-BcCsS2Y",
-  authDomain: "literate-d3e2f.firebaseapp.com",
-  projectId: "literate-d3e2f",
-  storageBucket: "literate-d3e2f.appspot.com",
-  messagingSenderId: "325451877505",
-  appId: "1:325451877505:web:c8b12a35f6560dcba15180",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
