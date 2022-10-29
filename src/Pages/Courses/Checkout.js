b4c9600aada8d1057b65e349323c4da0798ece11
nafisnihal <nafisnihal12@gmail.com>
2022-10-29 23:57:53 +0600

    literate final commit

--- src/Pages/Courses/Checkout.js
+++ src/Pages/Courses/Checkout.js
@@ -3,7 +3,6 @@ import { useLoaderData } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import { Button } from "react-bootstrap";
 import { FaDollarSign } from "react-icons/fa";
-import CardHeader from "react-bootstrap/esm/CardHeader";
 
 const Checkout = () => {
   const checkoutCourse = useLoaderData();
@@ -12,10 +11,8 @@ const Checkout = () => {
     <div className="w-50 mx-auto my-5">
       <Card className="text-dark">
         <Card.Header>{title}</Card.Header>
+        <Card.Header className="">Price: $99</Card.Header>
         <Card.Img src={image_url} alt={title} />
-        <Button variant="dark" className="w-25 mt-2 mx-auto" disabled>
-          Price: $99
-        </Button>
         <Button className="my-2 mx-4" variant="dark">
           Proceed To Payment <FaDollarSign className="pb-1"></FaDollarSign>
         </Button>
